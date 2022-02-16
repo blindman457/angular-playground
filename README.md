4e8e61011885d2e8ebf302d412d03f6eb2a7b521
blindman457 <blindman96@gmail.com>
2022-02-16 22:43:04 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-y3ae8u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-y3ae8u)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import {Product} from './products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  items: Product[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  addToCart(p: Product) {
+   this.items.push(p);
+  }
+  getItems() {
+   return  this.items;
+  }
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+}
